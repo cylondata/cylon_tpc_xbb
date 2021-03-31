28add3c91d101002f60093243a91226075fd3969
niranda perera <niranda.perera@gmail.com>
2021-03-31 09:49:43 -0400

    minor change

--- tpc_xbb/scripts/cylon_run.sh
+++ tpc_xbb/scripts/cylon_run.sh
@@ -7,14 +7,18 @@ CYLON_TPC_HOME="$HOME/romeo/git/cylon_tpc_xbb"
 python_bin=$(which python)
 
 #copy bencmark_config
-cp "$CYLON_TPC_HOME"/tpc_xbb/config/benchmark_config.yaml.template "$CYLON_TPC_HOME"/tpc_xbb/config/benchmark_config.yaml
+CONFIG_PATH="$CYLON_TPC_HOME"/tpc_xbb/config/benchmark_config.yaml
+if [ ! -f "$CONFIG_PATH" ]; then
+  echo "copying config"
+  cp "$CYLON_TPC_HOME"/tpc_xbb/config/benchmark_config.yaml.template $CONFIG_PATH
+fi
 
 for W in ${WORKERS[0]}; do
 
   for Q in ${QUERIES[0]}; do
     echo "query $Q starting"
 
-    mpirun -np ${W} ${python_bin} "$CYLON_TPC_HOME"/tpc_xbb/queries/q"$Q"/tpc_xbb_query_${Q}.py --config_file="$CYLON_TPC_HOME"/tpc_xbb/config/benchmark_config.yaml
+    mpirun -np ${W} ${python_bin} "$CYLON_TPC_HOME"/tpc_xbb/queries/q"$Q"/tpc_xbb_query_${Q}.py --config_file="$CONFIG_PATH"
 
     echo "query $Q done"
     echo "============="
