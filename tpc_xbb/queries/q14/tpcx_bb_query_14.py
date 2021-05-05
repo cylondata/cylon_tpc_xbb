a51017db3b50bb4220d68ec9c5f8a442d809bba6
niranda perera <niranda.perera@gmail.com>
2021-05-04 20:29:17 -0400

    fixing q14

--- tpc_xbb/queries/q14/tpcx_bb_query_14.py
+++ tpc_xbb/queries/q14/tpcx_bb_query_14.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ##
-
+from typing import Iterable
 
 import numpy as np
 import sys
@@ -21,30 +21,30 @@ from cylon_xbb_tools.utils import (
     tpcxbb_argparser
 )
 
+from pycylon.net import MPIConfig
+from pycylon import CylonEnv, DataFrame
 
-def read_tables(ctx, config):
-    table_reader = CSVReader(config["data_dir"],
-                             rank=None if ctx.get_world_size() == 1 else ctx.get_rank())
+
+def read_tables(env: CylonEnv, config) -> Iterable[DataFrame]:
+    table_reader = CSVReader(config["data_dir"], rank=None if env.world_size == 1 else env.rank)
 
     ws_columns = ["ws_ship_hdemo_sk", "ws_web_page_sk", "ws_sold_time_sk"]
-    web_sales = table_reader.read(ctx, "web_sales", relevant_cols=ws_columns)
+    web_sales = table_reader.read(env, "web_sales", relevant_cols=ws_columns)
 
     hd_columns = ["hd_demo_sk", "hd_dep_count"]
-    household_demographics_1part = table_reader.read(ctx,
-                                                     "household_demographics",
-                                                     relevant_cols=hd_columns
-                                                     )
+    household_demographics_1part = table_reader.read(env, "household_demographics",
+                                                     relevant_cols=hd_columns)
 
     wp_columns = ["wp_web_page_sk", "wp_char_count"]
-    web_page = table_reader.read(ctx, "web_page", relevant_cols=wp_columns)
+    web_page = table_reader.read(env, "web_page", relevant_cols=wp_columns)
 
     td_columns = ["t_time_sk", "t_hour"]
-    time_dim_1part = table_reader.read(ctx, "time_dim", relevant_cols=td_columns)
+    time_dim_1part = table_reader.read(env, "time_dim", relevant_cols=td_columns)
 
     return web_sales, household_demographics_1part, web_page, time_dim_1part
 
 
-def main(ctx, config):
+def main(env: CylonEnv, config):
     q14_dependents = 5
     q14_morning_startHour = 7
     q14_morning_endHour = 8
@@ -54,7 +54,7 @@ def main(ctx, config):
     q14_content_len_max = 6000
 
     web_sales, household_demographics_1part, web_page, time_dim_1part = read_tables(
-        ctx, config)
+        env, config)
 
     # print("web sales")
     # print(web_sales[0:10])
@@ -74,58 +74,45 @@ def main(ctx, config):
         # local_dict={"q14_dependents": q14_dependents},
     ]
 
-    output_table = web_sales.join(
-        household_demographics_1part,
-        left_on=["ws_ship_hdemo_sk"],
-        right_on=["hd_demo_sk"],
-        join_type="inner",
-        algorithm="sort"
-    )
+    output_table = web_sales.merge(household_demographics_1part, how="inner", algorithm="sort",
+                                   left_on=["ws_ship_hdemo_sk"], right_on=["hd_demo_sk"],
+                                   suffixes=('', ''))  # local
 
-    print("####", output_table.row_count)
+    # print("####", output_table.row_count)
 
     # print("After join")
     # print(output_table[0:10])
 
-    output_table = output_table.drop(
-        ["ws_ship_hdemo_sk", "hd_demo_sk", "hd_dep_count"])
+    output_table = output_table.drop(["ws_ship_hdemo_sk", "hd_demo_sk", "hd_dep_count"])
 
-    web_page = web_page[
-        (web_page["wp_char_count"] >= q14_content_len_min) &
-        (web_page["wp_char_count"] >= q14_content_len_min) &
-        (web_page["wp_char_count"] <= q14_content_len_max) &
-        (web_page["wp_char_count"] >= q14_content_len_min) &
-        (web_page["wp_char_count"] <= q14_content_len_max)]
+    web_page = web_page[(web_page["wp_char_count"] >= q14_content_len_min) &
+                        (web_page["wp_char_count"] >= q14_content_len_min) &
+                        (web_page["wp_char_count"] <= q14_content_len_max) &
+                        (web_page["wp_char_count"] >= q14_content_len_min) &
+                        (web_page["wp_char_count"] <= q14_content_len_max)]
 
-    output_table = output_table.join(
-        web_page, left_on=["ws_web_page_sk"], right_on=["wp_web_page_sk"], join_type="inner",
-        algorithm="sort"
-    )
+    output_table = output_table.merge(web_page, how="inner", algorithm="sort",
+                                      left_on=["ws_web_page_sk"], right_on=["wp_web_page_sk"],
+                                      suffixes=('', ''), env=env)
 
-    output_table = output_table.drop(
-        ["ws_web_page_sk", "wp_web_page_sk", "wp_char_count"])
+    output_table = output_table.drop(["ws_web_page_sk", "wp_web_page_sk", "wp_char_count"])
 
-    time_dim_1part = time_dim_1part[
-        (time_dim_1part["t_hour"] == q14_morning_startHour) |
-        (time_dim_1part["t_hour"] == q14_morning_endHour) |
-        (time_dim_1part["t_hour"] == q14_evening_startHour) |
-        (time_dim_1part["t_hour"] == q14_evening_endHour)
-        ]
+    time_dim_1part = time_dim_1part[(time_dim_1part["t_hour"] == q14_morning_startHour) |
+                                    (time_dim_1part["t_hour"] == q14_morning_endHour) |
+                                    (time_dim_1part["t_hour"] == q14_evening_startHour) |
+                                    (time_dim_1part["t_hour"] == q14_evening_endHour)]
 
-    output_table = output_table.join(
-        time_dim_1part, left_on=["ws_sold_time_sk"], right_on=["t_time_sk"], join_type="inner",
-        algorithm="sort"
-    )
+    output_table = output_table.merge(time_dim_1part, how="inner", algorithm="sort",
+                                      left_on=["ws_sold_time_sk"], right_on=["t_time_sk"],
+                                      suffixes=('', ''))  # local
 
     output_table = output_table.drop(["ws_sold_time_sk", "t_time_sk"])
 
-    output_table["am"] = (output_table["t_hour"] >= q14_morning_startHour) & (
-            output_table["t_hour"] <= q14_morning_endHour
-    )
+    output_table["am"] = (output_table["t_hour"] >= q14_morning_startHour) & \
+                         (output_table["t_hour"] <= q14_morning_endHour)
 
-    output_table["pm"] = (output_table["t_hour"] >= q14_evening_startHour) & (
-            output_table["t_hour"] <= q14_evening_endHour
-    )
+    output_table["pm"] = (output_table["t_hour"] >= q14_evening_startHour) & \
+                         (output_table["t_hour"] <= q14_evening_endHour)
 
     # print("output_table")
     # print(output_table[0:10])
@@ -133,29 +120,28 @@ def main(ctx, config):
     am_trues = output_table[output_table["am"] == True]
     pm_trues = output_table[output_table["pm"] == True]
 
-    am_pm_ratio = am_trues.row_count / pm_trues.row_count
+    # get the distributed count of these tables
+    am_pm_ratio = am_trues.to_table().count(0) / pm_trues.to_table().count(0) # table API
 
-    if np.isinf(am_pm_ratio):
-        am_pm_ratio = -1.0
+    np_am_pm_ratio = am_pm_ratio.to_numpy()[0,0]
+    if np.isinf(np_am_pm_ratio):
+        np_am_pm_ratio = -1.0
 
-    print(am_pm_ratio)
+    print(np_am_pm_ratio)
 
     from pycylon import Table
-    return Table.from_pydict(ctx, {"am_pm_ratio": [am_pm_ratio]})
+    return Table.from_pydict(env, {"am_pm_ratio": [np_am_pm_ratio]})
 
 
 if __name__ == "__main__":
     config = tpcxbb_argparser()
 
-    from pycylon import CylonContext
-    from pycylon.net import MPIConfig
-
     mpi_config = MPIConfig()
-    ctx: CylonContext = CylonContext(config=mpi_config, distributed=True)
+    ctx: CylonEnv = CylonEnv(config=mpi_config, distributed=True)
 
     res = main(ctx, config)
 
-    if ctx.get_rank() == 0:
+    if ctx.rank == 0:
         import os
 
         os.makedirs(config['output_dir'], exist_ok=True)
